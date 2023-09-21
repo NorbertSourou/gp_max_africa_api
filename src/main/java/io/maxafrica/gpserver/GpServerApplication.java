f8c028de4a39951cb59e45c1ae81ab129b1c7a5d
Norbert Souwouin <souwouinnorbert@gmail.com>
2023-09-21 19:43:46 +0100

    Initial commit

---
+++ src/main/java/io/maxafrica/gpserver/GpServerApplication.java
@@ -0,0 +1,13 @@
+package io.maxafrica.gpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpServerApplication.class, args);
+	}
+
+}
